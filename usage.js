8a0f9815858833c060b29e545e1d50bbf54c8e48
BR <bjorn.roberg@gmail.com>
2016-07-07 23:39:44 +0200

    umd, usage example from consumer side

---
+++ usage.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  window.zoomify({
+    container: document.getElementById('image-zoom-wrapper'),
+    img: document.getElementById('image-zoom')
+  });
+});
